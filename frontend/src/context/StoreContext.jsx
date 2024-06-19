fe70cee0d0c3834710edc5a29aa504fb2e2fb564
Vishva Prakash Maddheshiya <126156593+vishvaprakash24@users.noreply.github.com>
2024-06-19 15:35:09 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "http://localhost:10000";
   const [token, setToken] = useState("")
   const [food_list, setFoodList] = useState([])
 
